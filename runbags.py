bc6838cd3b81bd93ca49f33b67b5dfb1b9584d12
Hans Hinrichsen <hinrichsen.hans@gmail.com>
2017-06-19 18:20:03 +0000

    Added player1 player2 selection

--- runbags.py
+++ runbags.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python
 from OSC import *
 import RPi.GPIO as GPIO
+import sys
+
+print 'Started with arguments:', str(sys.argv)
+print 'Mode: ', sys.argv[1]
+
+if sys.argv[1] == 'player1' :
+	outmsg = "/eos/fader/1/2/fire"
+else :
+	outmsg = "/eos/fader/1/5/fire"
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.IN)
@@ -19,7 +28,7 @@ def send_osc_on_change(channel) :
 		# c.sendOSC(OSCMessage(""))
 		GPIO.output(25, False)
 	else :
-		c.sendOSC(OSCMessage("/eos/fader/1/2/fire"))
+		c.sendOSC(OSCMessage(outmsg))
 		GPIO.output(25, True)
 
 GPIO.add_event_detect(27, GPIO.BOTH, callback=send_osc_on_change, bouncetime=1)
