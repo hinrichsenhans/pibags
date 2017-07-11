c13c22bb648dcc01be22be3b3256aa5e56f48920
Hans Hinrichsen <hinrichsen.hans@gmail.com>
2017-07-11 01:32:11 +0000

    Added variables rather than constants, fix init conditions

--- runbags.py
+++ runbags.py
@@ -5,32 +5,46 @@ import RPi.GPIO as GPIO
 import sys
 import time 
 
-print('Started with arguments:', str(sys.argv))
-print('Mode: ', sys.argv[1])
+IR_INPUT_PIN = 6 
+LED_OUTPUT_PIN = 25
+PRINT_STATE_CHANGE = 0
 
-if sys.argv[1] == 'player1' :
+print('Started with arguments:', str(sys.argv))
+
+if 'player1' in sys.argv :
 	outmsg = "/eos/fader/1/2/fire"
+	print('Mode: Player 1')
 else :
 	outmsg = "/eos/fader/1/5/fire"
+	print('Mode: Player 2')
+outmsg = "/eos/fader/1/8/fire" 
+# ignore for now
+inimsg = "/eos/fader/1/config/10"
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(27, GPIO.IN)
-GPIO.setup(25, GPIO.OUT)
-
+GPIO.setup(IR_INPUT_PIN, GPIO.IN)
+GPIO.setup(LED_OUTPUT_PIN, GPIO.OUT)
 osc_startup()
+
 osc_udp_client("10.101.2.177", 53001, "hans_etcnomad")
 
 try:
 	msg = oscbuildparse.OSCMessage(outmsg, "", [])
+	initmsg = oscbuildparse.OSCMessage(inimsg, "", {})
+	osc_send(initmsg, "hans_etcnomad")
 
 	def send_osc_on_change(channel) :
 		if GPIO.input(channel):
-			GPIO.output(25, False)
+			GPIO.output(LED_OUTPUT_PIN, False)
+			if PRINT_STATE_CHANGE :
+				print("off\n")
 		else :
-			GPIO.output(25, True)
+			GPIO.output(LED_OUTPUT_PIN, True)
 			osc_send(msg, "hans_etcnomad")
+			if PRINT_STATE_CHANGE :
+				print("on\n")
 	
-	GPIO.add_event_detect(27, GPIO.BOTH, callback=send_osc_on_change, bouncetime=1)
+	GPIO.add_event_detect(IR_INPUT_PIN, GPIO.BOTH, callback=send_osc_on_change, bouncetime=1)
 
 	while True:
 		osc_process()
